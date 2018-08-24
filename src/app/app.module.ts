6c79742b3a4551ea9e9095a7fe6678d2120c4c1d
Joseph.Dibi <jjdibiasi13@gmail.com>
2018-08-24 13:12:20 -0600

    added resume and about me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 
 
@@ -13,12 +14,16 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AccordionComponent } from './accordion/accordion.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AccordionComponent,
+    ResumeComponent,
+    ContactInfoComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +35,8 @@ import { AccordionComponent } from './accordion/accordion.component';
     MatIconModule,
     MatFormFieldModule,
     MatTabsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    PdfViewerModule
   ],
   providers: [],
   entryComponents: [AccordionComponent],
